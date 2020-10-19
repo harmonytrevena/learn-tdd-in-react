b6afd2ac9e3abcf2ff2e17af0934335869d962ad
Harmony Trevena <harmonytrevena@gmail.com>
2020-10-19 13:00:16 -0400

    Test-Driven Development in React for a simple list of messages.

---
+++ src/components/MessageList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MessageList = props => {
+    const { messages } = props;
+    return (
+        <ul>
+            {messages.map((message, index) => (
+                <li key={`message-${index}`}>{message}</li>
+            ))}
+        </ul>
+    )
+}
+export default MessageList;
---
+++ src/components/NewMessageForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const NewMessageForm = ({onSend}) => {
+    const [inputText, setInputText] = useState("");
+
+    const _handleChange = text => {
+        setInputText(text);
+    };
+
+    const _handleClick = () => {
+        onSend(inputText);
+        setInputText("");
+    };
+
+    return (
+        <div>
+            <input 
+                type="text" 
+                data-testid="messageText" 
+                value={inputText} 
+                onChange={(event) => _handleChange(event.target.value)}
+            />
+            <button 
+                type="button" 
+                data-testid="sendButton" 
+                onClick={(_handleClick)}>
+                    Send
+            </button>
+        </div>
+    )
+}
+
+export default NewMessageForm;
